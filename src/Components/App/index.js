462091118a06b03534dd2b0b0d7a0e973fc0711f
Hyosssssun <hyosssssun@gmail.com>
2022-02-12 21:40:06 +0000

    separated backend and frontend

---
+++ src/Components/App/index.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Input from '../Input';
+import List from '../List';
+import Item from '../Item';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hi it is project tracker</h1>
+      <Input />
+      <List />
+      <Item />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Item/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Item() {
+    return <h2>Hi i am Item</h2>;
+}
